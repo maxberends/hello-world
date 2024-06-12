5d46284228700b52cef4338bd58ef2c7f82ef2b8
maxberends <maxberends001@gmail.com>
2024-06-12 11:48:49 +0200

    Update ds18.ino

--- pid/ds18.ino
+++ pid/ds18.ino
@@ -1,5 +1,19 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <PID_v1.h>
+
+#define PIN_INPUT 0
+#define RELAY_PIN 6
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+int WindowSize = 5000;
+unsigned long windowStartTime;
 
 // GPIO where the DS18B20 is connected to
 const int oneWireBus = 4;     
@@ -15,6 +29,17 @@ void setup() {
   Serial.begin(115200);
   // Start the DS18B20 sensor
   sensors.begin();
+
+  windowStartTime = millis();
+
+  //initialize the variables we're linked to
+  Setpoint = 100;
+
+  //tell the PID to range between 0 and the full window size
+  myPID.SetOutputLimits(0, WindowSize);
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -25,5 +50,19 @@ void loop() {
   Serial.println("ºC");
   Serial.print(temperatureF);
   Serial.println("ºF");
+
+  Input = analogRead(PIN_INPUT);
+  myPID.Compute();
+
+  /************************************************
+   * turn the output pin on/off based on pid output
+   ************************************************/
+  if (millis() - windowStartTime > WindowSize)
+  { //time to shift the Relay Window
+    windowStartTime += WindowSize;
+  }
+  if (Output < millis() - windowStartTime) digitalWrite(RELAY_PIN, HIGH);
+  else digitalWrite(RELAY_PIN, LOW);
+  
   delay(5000);
 }
